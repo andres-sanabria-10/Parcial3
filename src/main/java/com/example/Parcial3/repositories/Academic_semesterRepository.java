7c4ac0174381b7a1fb1d8a510787ebf10f33d1e3
JhonatanGuarin <jhonatancamilo99@gmail.com>
2024-07-31 02:23:31 -0500

    Service y Controller

--- src/main/java/com/example/Parcial3/repositories/Academic_semesterRepository.java
+++ src/main/java/com/example/Parcial3/repositories/Academic_semesterRepository.java
@@ -3,9 +3,24 @@ package com.example.Parcial3.repositories;
 import com.example.Parcial3.model.Academic_semester;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import  com.example.Parcial3.model.PracticalIntersectionCompany;
 
+import java.util.List;
+
 @Repository
 public interface Academic_semesterRepository extends JpaRepository<Academic_semester, Integer> {
+
+
+    @Query("SELECT as.name as semesterName, m.name as matterName, " +
+            "t.name as teacherName, t.lastName as teacherLastName, " +
+            "p.startDate, p.endDate, p.location " +
+            "FROM Academic_semester as " +
+            "JOIN as.matter m " +
+            "JOIN as.teacher t " +
+            "JOIN m.practice p " +
+            "WHERE as.id = :semesterId")
+    static List<Object[]> findPracticesSummaryBySemester(@Param("semesterId") Integer semesterId);
+
 }
--- src/main/java/com/example/Parcial3/services/PracticeService.java
+++ src/main/java/com/example/Parcial3/services/PracticeService.java
@@ -3,11 +3,14 @@ package com.example.Parcial3.services;
 import com.example.Parcial3.model.Practice;
 import com.example.Parcial3.model.Student;
 import com.example.Parcial3.repositories.PracticeRepository;
+import com.example.Parcial3.repositories.Academic_semesterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PracticeService {
@@ -37,6 +40,19 @@ public class PracticeService {
 
     public List<Student> getStudentsByPracticeId(Integer practiceId) {
         return practiceRepository.findStudentsByPracticeId(practiceId);
+    }
+
+    public List<Map<String, Object>> getPracticesSummaryBySemester(Integer semesterId) {
+        List<Object[]> results = Academic_semesterRepository.findPracticesSummaryBySemester(semesterId);
+
+        return results.stream().map(row -> Map.of(
+                "semesterName", row[0],
+                "matterName", row[1],
+                "teacherName", row[2] + " " + row[3], // Combinamos nombre y apellido del profesor
+                "startDate", row[4],
+                "endDate", row[5],
+                "location", row[6]
+        )).collect(Collectors.toList());
     }
 
     public Practice updatePractice(Integer id, Practice practiceDetails) {
